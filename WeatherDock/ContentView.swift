0450a086c0fd8a1ef47691cca4c6fc003eee07ee
Alexander Stepanischev <stepanishchev@gmail.com>
2022-05-16 15:07:04 +0200

    Initial Commit

---
+++ WeatherDock/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeatherDock
+//
+//  Created by Aleksandr Stepanischev on 16/05/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeatherDock/WeatherDockApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherDockApp.swift
+//  WeatherDock
+//
+//  Created by Aleksandr Stepanischev on 16/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherDockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
