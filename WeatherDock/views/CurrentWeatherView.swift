7c9a681d87d3e6755a32030e499e4e6a13d15efb
Alexander Stepanischev <stepanishchev@gmail.com>
2022-07-20 12:29:42 +0200

    Added pollutants info popover

--- WeatherDock/views/CurrentWeatherView.swift
+++ WeatherDock/views/CurrentWeatherView.swift
@@ -11,6 +11,7 @@ struct CurrentWeatherView: View {
     var data: CurrentWeatherData
     var aqi: AirPollutionData
     @Binding var updater: Bool
+    @State var isPopover = false
     
     var body: some View {
         VStack{
@@ -45,7 +46,7 @@ struct CurrentWeatherView: View {
                         .help("Humidity")
                     Text("\(data.main.humidity)%").font(.headline)
                         .help("Humidity")
-                }.padding(.trailing)
+                }.padding(.horizontal)
                 HStack{
                     Image(systemName: "wind").font(Font.system(size: 15, weight: .bold))
                         .help("Wind speed")
@@ -63,8 +64,60 @@ struct CurrentWeatherView: View {
                         .help("Air quality")
                     Text(Utils.aqi[aqi.list[0].main.aqi]?.0 ?? "Unknown").font(.headline)
                         .help("Air quality")
+                        .padding(.trailing)
+                }.popover(isPresented: self.$isPopover, arrowEdge: .trailing) {
+                    AQPopoverView(components: aqi.list[0].components)
+                }
+                .onHover { hover in
+                    self.isPopover = hover
                 }
             }.padding(.top, 1)
         }
+    }
+}
+
+struct AQPopoverView: View {
+    var components: AirComponents
+    var body: some View {
+        VStack {
+            Text("Pollutants").font(.title3).bold()
+            Text("μg/m").font(.subheadline).italic() + Text("3").font(.system(size: 7.0)).italic().baselineOffset(5.0)
+            VStack(alignment: .leading){
+                HStack{
+                    CardView(label: "PM2.5", value: String(format: "%.1f", components.pm2_5 ?? 0.0))
+                    CardView(label: "PM10", value: String(format: "%.1f", components.pm10 ?? 0.0))
+                }
+                HStack{
+                    CardView(label: "CO", value: String(format: "%.1f", components.co ?? 0.0))
+                    CardView(label: "O3", value: String(format: "%.1f", components.o3 ?? 0.0))
+                }
+                HStack{
+                    CardView(label: "NO", value: String(format: "%.1f", components.no ?? 0.0))
+                    CardView(label: "NO2", value: String(format: "%.1f", components.no2 ?? 0.0))
+                }
+                HStack{
+                    CardView(label: "SO2", value: String(format: "%.1f", components.so2 ?? 0.0))
+                    CardView(label: "NH3", value: String(format: "%.1f", components.nh3 ?? 0.0))
+                }
+            }.padding(.top, 1)
+        }.padding()
+    }
+}
+
+struct CardView: View {
+    var label: String
+    var value: String
+    
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 10, style: .continuous)
+                .stroke(lineWidth: 1)
+
+            VStack {
+                Text(label).font(.headline)
+                Text(value)
+            }.padding(5)
+            .multilineTextAlignment(.center)
+        }.frame(width: 60)
     }
 }
